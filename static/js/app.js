53cfdfcb694eabdc8b4315bc80ece18d16d232bc
Justan <jplumlee@outlook.com>
2020-10-11 13:31:02 -0700

    Updated Bar Chart code

--- static/js/app.js
+++ static/js/app.js
@@ -1,45 +1,64 @@
-function metaData() {
+function metaData(id) {
 
     d3.json("samples.json").then(function(data) {
         var demoData = data.metadata;
-        var filteredData = demoData.filter(sample => sample.id === 940);
+        var filteredData = demoData.filter(sample => sample.id.toString() === id)[0];
         var sampleMetadata = d3.select("#sample-metadata");
         
         // Use `.html("") to Clear any Existing Metadata
         sampleMetadata.html("");
 
         Object.entries(filteredData).forEach(([key, value]) => {
-          sampleMetadata.append("p").text(`${key}:${value}`);
+          sampleMetadata.append("h5").text(`${key}:${value}`);
         })
 
     });
 };
 
-function charts(sampleId) {
+function charts() {
     d3.json("samples.json").then(function(importedData) {
         var data = importedData;
         var samples = data.samples;
+
+        const all_otu_ids = samples[0].otu_ids;
+        const all_sample_values = samples[0].sample_values;
+        const all_otu_labels = samples[0].otu_labels;
         // var sampleIds = samples.map(id => id);
         // var idIndex = sampleIds.findIndex(sampleId);
         const otu_ids = samples.map(({ otu_ids }) => otu_ids.slice(0, 10))[0];
-        const otu_ids2 = otu_ids.map(x => "OTU" + " " + x)
+        const otu_ids2 = otu_ids.map(x => "OTU" + " " + x);
         const otu_labels = samples.map(({ otu_labels }) => otu_labels.slice(0, 10))[0];
         const sample_values = samples.map(({ sample_values }) => sample_values.slice(0, 10))[0];
 
-        var bubbleData = [{
+        var barData = [{
             x: sample_values.reverse(),
             y: otu_ids2.reverse(),
-            // mode: "markers",
             text: otu_labels.reverse(),
             type: "bar",
             orientation: "h"
-            // marker: {
-            //     size: sample_values,
-            //     color: otu_ids,
-            // }
         }]
-    Plotly.newPlot("bar", bubbleData);
+    
+    Plotly.newPlot("bar", barData);
+
+        var bubbleData = [{
+            x: all_otu_ids,
+            y: all_sample_values,
+            text: all_otu_labels,
+            mode: "markers",
+            marker: {
+                size: all_sample_values,
+                color: all_otu_ids,
+            }
+        }]
+    
+    Plotly.newPlot("bubble", bubbleData);            
+
 });
+}
+
+function optionChanged(id) {
+    charts(id);
+    metaData(id);
 }
 
 
@@ -55,10 +74,9 @@ function dropdown() {
             dropdownMenu.append("option").text(sample).property("value", sample);
 
         });
-        // const defaultSample = data[0].id;
-        charts(dropdownMenu.property("value"));
+        metaData(data.names[0]);
+        charts(data.names[0]);
     });    
 };
 
 dropdown();
-charts();
